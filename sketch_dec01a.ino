9367077c6eea4a4e52b3e6081bedd6232a0dfb95
Rob Culliton <rob.culliton@gmail.com>
2017-12-01 20:03:26 -0700

    second bar

--- sketch_dec01a.ino
+++ sketch_dec01a.ino
@@ -18,17 +18,19 @@
 
 // notes in the melody:
 int melody[] = {
-  NOTE_B4, NOTE_D4, NOTE_E4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4
+  NOTE_B4, NOTE_D4, NOTE_E4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4,
+  NOTE_B4, NOTE_E4, NOTE_FS4,NOTE_FS4,NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4,NOTE_E4
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  1, 2, 1, 1, 2, 2, 2, 2, 1, 1
+  1, 2, 1, 1, 2, 2, 2, 2, 1, 1,
+  1, 1, 1, 1, 1, 2, 2, 2, 1
 };
 
 void setup() {
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 10; thisNote++) {
+  for (int thisNote = 0; thisNote < 19; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
